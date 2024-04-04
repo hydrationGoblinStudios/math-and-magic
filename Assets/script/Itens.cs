fd27b2e9581b0b321008a3c50981922844197049
SuperMegaGamerVerdadeiro <bernardobressan10@gmail.com>
2024-04-04 15:09:49 -0300

    Script de itens só o começo pq n o rafa pediu pra eu dar pull pra colocar os sprites dos inimigos

---
+++ Assets/script/Itens.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ovo Item", menuName = "Item")]
+
+public class Itens : ScriptableObject
+{
+    public string nome = "Sample Text"; 
+    public Sprite Icone = null;
+}
